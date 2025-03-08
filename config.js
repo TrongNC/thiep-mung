db9f759c636ccba869b286a37db8de34dc33b573
TrongNC <65897210+TrongNC@users.noreply.github.com>
2025-03-08 17:22:38 +0700

    Add files via upload
    Create card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Em';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Tics Ngáo';
+const contentCard = 'You are a flower in the making. Nurture yourself with love, stay close to those who truly care for you, and one day, you will bloom into your most radiant self at the perfect time.';
+
+const giftImage = 'tics.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
